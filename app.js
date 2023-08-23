3e9287b06f0a3a8ee7c2c66207674b081b39cf56
ivanpesantez <ivanpesantez.ing@gmail.com>
2023-08-23 07:59:17 -0500

    Level 2 - encryption v1.1

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -24,9 +25,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-const secret = "Thisisourlittlesecret";
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 //-------- LEVEL 2 SECURITY ---------//
 
